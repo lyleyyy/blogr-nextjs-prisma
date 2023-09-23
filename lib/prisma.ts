0ebcd06648dd3d601615d60dce15e5a3301ded21
lyleyang <lyletwro@gmail.com>
2023-09-23 14:41:02 +1000

    step 6 finished

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 
+// let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+
+// export default prisma;
+
 let prisma: PrismaClient;
 
+const generatedPrisma = new PrismaClient();
+
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  // prisma = new PrismaClient();
+  prisma = generatedPrisma;
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    // global.prisma = new PrismaClient();
+    global.prisma = generatedPrisma;
   }
+
   prisma = global.prisma;
 }
 
