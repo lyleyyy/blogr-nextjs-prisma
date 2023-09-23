1007235559362ffd73c72190a64f46b6acc427f1
lyleyang <lyletwro@gmail.com>
2023-09-23 20:15:18 +1000

    step 10 finished

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,9 +1,25 @@
-import React from "react";
-import { GetServerSideProps } from "next";
-import ReactMarkdown from "react-markdown";
-import Layout from "../../components/Layout";
-import { PostProps } from "../../components/Post";
-import prisma from "../../lib/prisma";
+// import React from "react";
+// import { GetServerSideProps } from "next";
+// import ReactMarkdown from "react-markdown";
+// import Layout from "../../components/Layout";
+// import { PostProps } from "../../components/Post";
+// import prisma from "../../lib/prisma";
+
+// // export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+// //   const post = await prisma.post.findUnique({
+// //     where: {
+// //       id: String(params?.id),
+// //     },
+// //     include: {
+// //       author: {
+// //         select: { name: true, email: true },
+// //       },
+// //     },
+// //   });
+// //   return {
+// //     props: post,
+// //   };
+// // };
 
 // export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 //   const post = await prisma.post.findUnique({
@@ -21,6 +37,55 @@ import prisma from "../../lib/prisma";
 //   };
 // };
 
+// const Post: React.FC<PostProps> = (props) => {
+//   let title = props.title;
+//   if (!props.published) {
+//     title = `${title} (Draft)`;
+//   }
+
+//   return (
+//     <Layout>
+//       <div>
+//         <h2>{title}</h2>
+//         <p>By {props?.author?.name || "Unknown author"}</p>
+//         <ReactMarkdown children={props.content} />
+//       </div>
+//       <style jsx>{`
+//         .page {
+//           background: white;
+//           padding: 2rem;
+//         }
+
+//         .actions {
+//           margin-top: 2rem;
+//         }
+
+//         button {
+//           background: #ececec;
+//           border: 0;
+//           border-radius: 0.125rem;
+//           padding: 1rem 2rem;
+//         }
+
+//         button + button {
+//           margin-left: 1rem;
+//         }
+//       `}</style>
+//     </Layout>
+//   );
+// };
+
+// export default Post;
+
+import React from "react";
+import { GetServerSideProps } from "next";
+import ReactMarkdown from "react-markdown";
+import Router from "next/router";
+import Layout from "../../components/Layout";
+import { PostProps } from "../../components/Post";
+import { useSession } from "next-auth/react";
+import prisma from "../../lib/prisma";
+
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
     where: {
@@ -37,7 +102,20 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   };
 };
 
+async function publishPost(id: string): Promise<void> {
+  await fetch(`/api/publish/${id}`, {
+    method: "PUT",
+  });
+  await Router.push("/");
+}
+
 const Post: React.FC<PostProps> = (props) => {
+  const { data: session, status } = useSession();
+  if (status === "loading") {
+    return <div>Authenticating ...</div>;
+  }
+  const userHasValidSession = Boolean(session);
+  const postBelongsToUser = session?.user?.email === props.author?.email;
   let title = props.title;
   if (!props.published) {
     title = `${title} (Draft)`;
@@ -49,10 +127,13 @@ const Post: React.FC<PostProps> = (props) => {
         <h2>{title}</h2>
         <p>By {props?.author?.name || "Unknown author"}</p>
         <ReactMarkdown children={props.content} />
+        {!props.published && userHasValidSession && postBelongsToUser && (
+          <button onClick={() => publishPost(props.id)}>Publish</button>
+        )}
       </div>
       <style jsx>{`
         .page {
-          background: white;
+          background: var(--geist-background);
           padding: 2rem;
         }
 
